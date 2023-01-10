262ec7338a092e81460417cf9ff6087270e7d39f
jnicomedes <Joao.PauloCamposNicomedes-ext@ab-Inbev.com>
2023-01-10 12:55:53 -0300

    Initial Commit

---
+++ app/src/main/java/com/jnicomedes/myapplication/ui/fragment/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.jnicomedes.myapplication.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jnicomedes.myapplication.R
+import com.jnicomedes.myapplication.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
